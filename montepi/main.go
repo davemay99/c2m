2538ba859a7e36579728a72fa6152b6cedae0cec
davemay99 <dmay@hashicorp.com>
2021-01-11 11:34:55 -0500

    apply linter suggestion

--- montepi/main.go
+++ montepi/main.go
@@ -41,7 +41,7 @@ func montepi(niter int) {
 	}
 
 	pi := float64(count) / float64(niter) * float64(4)
-	total := time.Now().Sub(start)
+	total := time.Since(start)
 
 	tps := float64(niter) / total.Seconds()
 	fmt.Printf("%s # of trials= %d (%f/s), estimate of pi is %g \n", total, niter, tps, pi)
